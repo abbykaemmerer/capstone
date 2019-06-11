c5d0bd76b5306aae8c3564af260a4f6153de4903
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-11 18:48:52 -0500

    Adding a Header.js component

--- components/Navigation.js
+++ components/Navigation.js
@@ -10,8 +10,7 @@ function buildNavHTML(stateLinks){
 
 export default (state) => `
 <nav>
-<header>${state.title}</header>
-<h3>Your one stop shop for products that make your pet feel puurfect.</h3>
+
 <ul class="nav-menu">
 <li>
 Menu
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+import Header from './components/Header';
 import Navigation from './components/Navigation';
-
 import Main from './components/Main';
-
 import Footer from './components/Footer';
-
 import * as states from './store';
 
 const root = document.querySelector('#root');
@@ -11,6 +9,7 @@ const root = document.querySelector('#root');
 function render(state){
     console.log('named param state is', state);
     root.innerHTML = `
+${Header(state)}
 ${Navigation(state)}
 ${Main(state)}
 ${Footer(state)}
