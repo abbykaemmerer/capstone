b88c401c188fbb2de12b50c5aa862acbcb22a895
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-12 09:42:47 -0500

    Adding photo to Cat page

--- store/Cat.js
+++ store/Cat.js
@@ -1,10 +1,11 @@
 import links from './Links';
 
-import image from '../docs/cat-page-header';
+import img from '../docs/cat-page-header.jpg';
 
 export default {
     'pageContent': 'Cat',
     'title': 'Cat Page Title',
     'subtitle': 'Cats',
+    'image': img,
     'links': links
 };
