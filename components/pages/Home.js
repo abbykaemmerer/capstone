ec1a5d055e96d11abeb8cf8d50c60be398f419fe
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-12 10:48:57 -0500

    Using flex to display the cat/dog buttons and adjusting footer padding

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -1,6 +1,6 @@
 export default (state) => `
 <main>
-    <div class="home-grid-container">
+    <div class="home-container">
         <div class="cats-button"><a href="catpage.html">CATS</a></div>
         <div class="dogs-button"><a href="dogpage.html">DOGS</a></div>
       </div>
