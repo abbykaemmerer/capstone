2b20ac78c726290630cfe0518448127d41a84e2d
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-11 20:41:35 -0500

    Lowercasing classes

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -1,7 +1,7 @@
 export default (state) => `
 <main>
     <div class="home-grid-container">
-        <div class="CATS-button"><a href="catpage.html">CATS</a></div>
-        <div class="DOGS-button"><a href="dogpage.html">DOGS</a></div>
+        <div class="cats-button"><a href="catpage.html">CATS</a></div>
+        <div class="dogs-button"><a href="dogpage.html">DOGS</a></div>
       </div>
 </main>`;
