a33500ba2ee3c56e6120814ffeabb8b4a4064fb7
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-21 15:22:58 -0500

    Formatting code to be more readable

--- components/pages/Cat.js
+++ components/pages/Cat.js
@@ -1,26 +1,28 @@
 export default (state) => `    <main>
+<div class="page-grid-container">
+  <div class="description">
+    <h1>${state.subtitle}</h1>
+      <p>Essential tips for raising a healthy cat:
+      <ul>
+          <li>The BASICS: food, water, a litter box</li>
+          <li>Cats are naturally playful- toys, catnip, and windows are all great enrichment</li>
+          <li>Cats will overeat if given the opportunity, feed them an appropriate amount</li>
+          <li>A diet consisting of dry AND wet food is recommended by most veterinarians </li>
+          <li>Cleanliness is very important to cats- they're weird- their food bowls, litter boxes, water all must be clean</li>
+      </ul>
+      If you think your cat might have needs beyond the scope of our essential tips, check below for common illnesses and conditions that affect the health of many cats.
+      </p>
+  </div>
 
-
-    <div class="description"> <h1>${state.subtitle}</h1> <p>Essential tips for raising a healthy cat:
-    <ul>
-      <li>The BASICS: food, water, a litter box</li>
-      <li>Cats are naturally playful- toys, catnip, and windows are all great enrichment</li>
-      <li>Cats will overeat if given the opportunity, feed them an appropriate amount</li>
-      <li>A diet consisting of dry AND wet food is recommended by most veterinarians </li>
-      <li>Cleanliness is very important to cats- they're weird- their food bowls, litter boxes, water all must be clean</li>
-    </ul>
-    If you think your cat might have needs beyond the scope of our essential tips, check below for common illnesses and conditions that affect the health of many cats.
-
-  </p></div>
     <div class="links">
       <div id='conditions1'>
-      <a href="./ObesityCat">Obesity</a>
-      <a href="./Diabetescat">Diabetes</a>
-      <a href="./Ut">Urinary Tract</a>
+        <a href="./ObesityCat">Obesity</a>
+        <a href="./Diabetescat">Diabetes</a>
+        <a href="./Ut">Urinary Tract</a>
       </div>
       <div id= 'conditions2'>
-      <a href="./Hairballs">Hair Balls</a>
-      <a href="./Pregcat">Pregnancy</a>
+        <a href="./Hairballs">Hair Balls</a>
+        <a href="./Pregcat">Pregnancy</a>
       </div>
     </div>
     <div class="pictures">
@@ -28,5 +30,5 @@ export default (state) => `    <main>
       <img src="${state.image2}" alt="">
       <img src="${state.image3}".jpg" alt="">
     </div>
-
+</div>
 </main>`;
--- components/pages/Dog.js
+++ components/pages/Dog.js
@@ -1,32 +1,34 @@
 export default (state) => `      <main>
 <div class="page-grid-container">
-
-    <div class="description"> <h1>${state.subtitle}</h1><p>Essential tips for raising a healthy dog:
-    <ul>
-      <li>The BASICS: food, water, exercise</li>
-      <li>A high quality diet has an innumerable amount of positive health benefits for dogs- healthy skin, shiny coat, bright eyes, strong immune system</li>
-      <li>Exercise! Not only is it fun for your pet, but dogs that are overweight are at a much higher risk for disease and health problems than those at a healthy weight</li>
-      <li>If it's too hot/cold for you to be outside barefoot, it is for them too </li>
-      <li>Brush their teeth- they probably won't like it but they won't like losing teeth either</li>
-    </ul>
+  <div class="description">
+      <h1>${state.subtitle}</h1>
+        <p>Essential tips for raising a healthy dog:
+        <ul>
+          <li>The BASICS: food, water, exercise</li>
+          <li>A high quality diet has an innumerable amount of positive health benefits for dogs- healthy skin, shiny coat, bright eyes, strong immune system</li>
+          <li>Exercise! Not only is it fun for your pet, but dogs that are overweight are at a much higher risk for disease and health problems than those at a healthy weight</li>
+          <li>If it's too hot/cold for you to be outside barefoot, it is for them too </li>
+          <li>Brush their teeth- they probably won't like it but they won't like losing teeth either</li>
+      </ul>
     If you think your dog might have needs beyond the scope of our essential tips, check below for common illnesses and conditions that affect the health of many dogs.
+      </p>
+  </div>
 
-  </p></div>
-    <div class="links">
+  <div class="links">
       <div id='conditions1'>
-      <a href="./Dogobesity">Obesity</a>
-      <a href="./Skin">Poor Skin & Coat Condition</a>
-      <a href="./Pancreatitis">Pancreatitis</a>
+        <a href="./Dogobesity">Obesity</a>
+        <a href="./Skin">Poor Skin & Coat Condition</a>
+        <a href="./Pancreatitis">Pancreatitis</a>
       </div>
       <div id= 'conditions2'>
-      <a href="./Dogdiabetes">Diabetes</a>
-      <a href="./Dogpreg">Pregnancy</a>
+        <a href="./Dogdiabetes">Diabetes</a>
+        <a href="./Dogpreg">Pregnancy</a>
       </div>
-    </div>
-    <div class="pictures">
-    <img src="${state.image1}" alt="">
-    <img src="${state.image2}" alt="">
-    <img src="${state.image3}"  alt="">
-    </div>
   </div>
+    <div class="pictures">
+      <img src="${state.image1}" alt="">
+      <img src="${state.image2}" alt="">
+      <img src="${state.image3}"  alt="">
+    </div>
+</div>
 </main>`;
