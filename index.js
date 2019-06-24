3404e5c5731c057235e9f30c8c90be7aa4ef9081
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-24 15:04:21 -0500

    Commenting out code and using new method for product button event listener

--- index.js
+++ index.js
@@ -44,52 +44,54 @@ router
     .resolve();
 
 
-const prodButton = document.querySelector('#prodButton');
+const prodButton = document.querySelectorAll('#prodButton');
 
+prodButtons.forEach((prodButton) => {
+    prodButton.querySelector('button').addEventListener('click', () => console.log('click!'));
+});
 
-prodButton.addEventListener('click', (() => {
-    const img = states.Obesitycat.productOne.image;
+// prodButton.addEventListener('click', (() => {
+//     const img = states.Obesitycat.productOne.image;
 
-    const name = states.Obesitycat.productOne.name;
-    const price = states.Obesitycat.productOne.price;
-    const description = states.Obesitycat.productOne.description;
-    const qty = states.Obesitycat.productOne.qty;
-    const pageContent = states.Obesitycat.pageContent;
+// const name = states.Obesitycat.productOne.name;
+// const price = states.Obesitycat.productOne.price;
+// const description = states.Obesitycat.productOne.description;
+// const qty = states.Obesitycat.productOne.qty;
+// const pageContent = states.Obesitycat.productOne.pageContent;
 
-    if(typeof(Storage) !== 'undefined'){
-        const bill = [];
+// if(typeof(Storage) !== 'undefined') {
+//    const bill = [];
 
-        const receipt = {
-            'pageContent': pageContent,
-            'image': img,
-            'name': name,
-            'price': price,
-            'description': description,
-            'qty': qty
-        };
+// const receipt = {
+//     'pageContent': pageContent,
+//     'image': img,
+//     'name': name,
+//     'price': price,
+//     'description': description,
+//     'qty': qty
+// };
 
+//        bill.push(receipt);
+//        console.log(bill);
 
-        bill.push(receipt);
-        console.log(bill);
+//        localStorage.setItem('receipt', JSON.stringify(receipt));
 
-        localStorage.setItem('receipt', JSON.stringify(receipt));
+// Store
 
-        // Store
+// localStorage.setItem('image', img);
+// localStorage.setItem('name', name);
+// localStorage.setItem('price', price);
+// localStorage.setItem('qty', qty);
 
-        localStorage.setItem('image', img);
-        localStorage.setItem('name', name);
-        localStorage.setItem('price', price);
-        localStorage.setItem('qty', qty);
+// bill.push(receipt);
+// console.log(bill[0].description);
 
-        bill.push(receipt);
-        console.log(bill[0].description);
-
-        // Retrieve
-        // document.getElementById('result').innerHTML = localStorage.getItem('lastname');
-    }
-    // else{
-    //     document.getElementById('result').innerHTML = 'Sorry, your browser does not support Web Storage...';
-    // }
-})
-);
+// Retrieve
+// document.getElementById('result').innerHTML = localStorage.getItem('lastname');
+// }
+// else{
+//     document.getElementById('result').innerHTML = 'Sorry, your browser does not support Web Storage...';
+// }
+// })
+// );
 
