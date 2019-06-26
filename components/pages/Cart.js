ac84b7ee2ba941d7a4e3c9cb2252b99d383e50e3
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-25 22:34:13 -0500

    Attempting a function to allow as many products as are clicked to be displayed in cart

--- components/pages/Cart.js
+++ components/pages/Cart.js
@@ -1,18 +1,10 @@
 /* eslint-disable no-lone-blocks */
 const getObject = JSON.parse(localStorage.getItem('receipt'));
 
-export default () =>
-    `
-    ${console.log(getObject)};
-<main>
-
-<div class="shopping-cart">
-
-  <div class="shopping-cart-title">
-    Shopping Cart
-  </div>
-
-<div class="prod">
+function itemAdder(receipt){
+    return receipt.map((receipt,i) =>
+        `<div class="card" id="prod-${i + 1}">
+        <div class="prod">
     <div id = 'delete-button'>
       <button id="delete-button" type="button" name="button">
       <i class="fas fa-trash"></i>
@@ -32,7 +24,57 @@ export default () =>
         </button>
       </div>
 
-      <input type="text" name="name" value=${getObject.qty}>
+      <input type="text" name="name" value=1>
+
+      <div id = 'minus-button'>
+        <button id="minus-button" type="button" name="button">
+          <i class="fas fa-minus"></i>
+        </button>
+      </div>
+    </div>
+      `
+    ).join(' ');
+}
+
+export default () =>
+    `
+    ${console.log(getObject)};
+<main>
+
+<div class="shopping-cart">
+
+  <div class="shopping-cart-title">
+    Shopping Cart
+  </div>
+${itemAdder(receipt)}
+
+<div class="checkout-button">
+<a href="./Checkout"> Proceed to Checkout </a>
+</div>
+
+</main>`;
+
+{ /* <div class="prod">
+    <div id = 'delete-button'>
+      <button id="delete-button" type="button" name="button">
+      <i class="fas fa-trash"></i>
+      </button>
+    </div>
+    <div id = 'image'>
+      <img src="${getObject.image}" height = 100>
+    </div>
+    <div id = 'desc'>
+      <span><div id = 'name'>${getObject.name}</div></span>
+      <span>${getObject.description}</span>
+    </div>
+    <div id = 'qty'>
+      <div id = 'plus-button'>
+        <button id="plus-button" type="button" name="button">
+          <i class="fas fa-plus"></i>
+        </button>
+      </div>
+
+      <input type="text" name="name" value=1>
 
       <div id = 'minus-button'>
         <button id="minus-button" type="button" name="button">
@@ -43,87 +85,4 @@ export default () =>
 
     <div id = 'price'>${getObject.price}</div>
 
-</div>
-<div class="checkout-button">
-<a href="./Checkout"> Proceed to Checkout </a>
-</div>
-
-</main>`;
-
-{ /* <div class="item-image">
-<img src="${state.image2}" alt="" height="200" width="175"/>
-</div>
-
-<div class="item-description">
-<span>Product 1</span>
-<span>Description</span>
-</div>
-
-<div class="quantity">
-<button class="plus-button" type="button" name="button">
-  <i class="fas fa-plus"></i>
-</button>
-
-<input type="text" name="name" value="1">
-
-<button class="minus-button" type="button" name="button">
-<i class="fas fa-minus"></i>
-</button>
-</div>
-<div class="total-price">$2342</div>
-</div>
-
-
-<div class="item">
-
-<div class="item-image">
-  <img src="${state.image1}" alt="" height="200" width="175" />
-</div>
-
-<div class="item-description">
-    <span>Product 2</span>
-    <span>Description</>
-</div>
-
-<div class="quantity">
-  <button class="plus-button" type="button" name="button">
-    <i class="fas fa-plus"></i>
-  </button>
-
-<input type="text" name="name" value="1">
-
-<button class="minus-button" type="button" name="button">
-  <i class="fas fa-minus"></i>
-</button>
-</div>
-<div class="total-price">$22</div>
-</div>
-
-<div class="item">
-
-  <div class="item-image">
-    <img src="${state.image3}" alt="" height="200" width="175"/>
-  </div>
-
-  <div class="item-description">
-    <span>Product 3</span>
-    <span>Description</span>
-  </div>
-
-  <div class="quantity">
-    <button id="plusButton" type="button" name="button">
-      <i class="fas fa-plus"></i>
-    </button>
-
-  <input type="text" name="name" value="1">
-
-  <button id="minus-button" type="button" name="button">
-    <i class="fas fa-minus"></i>
-  </button>
-  </div>
-  <div class="total-price">$653</div>
-</div>
-<div class="checkout-button">
-<a href="./Checkout"> Proceed to Checkout </a>
-</div>
 </div> */ }
