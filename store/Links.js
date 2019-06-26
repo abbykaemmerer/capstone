09028278003f42f735aa82e76a1cb1855ac99575
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-26 13:01:40 -0500

    Add icons for nav

--- store/Links.js
+++ store/Links.js
@@ -1,5 +1,5 @@
 export default
 {
     'primary': [ { 'text': 'Menu' } ],
-    'dropdown': [ { 'text': 'Home' }, { 'text': 'Cat' }, { 'text': 'Dog' },  { 'text': 'Cart' } ]
+    'dropdown': [ { 'text': 'Home', 'icon': 'fas fa-paw' }, { 'text': 'Cat', 'icon': 'fas fa-cat' }, { 'text': 'Dog', 'icon': 'fas fa-dog' },  { 'text': 'Cart', 'icon': 'fas fa-shopping-cart' } ]
 };
