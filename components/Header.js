955312cede979eb6fbc1c34b8c0b06a549e022d7
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-26 22:53:57 -0500

    Add paw print to header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
 export default (state) => `<header>
-<h1>${state.title}</h1>
+<h1>${state.title}<i class="fas fa-paw" id="paw"></i></h1>
 <h3>An eCommerce site designed to help pet owners find the best products for their pets.</h3>
 </header>`;
