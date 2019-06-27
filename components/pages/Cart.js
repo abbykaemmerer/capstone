dd9c88c5e1ff74c86cb87c34047ca089e237a0a2
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-26 21:50:07 -0500

    Switching var to let in cart

--- components/pages/Cart.js
+++ components/pages/Cart.js
@@ -1,15 +1,11 @@
 
 function itemAdder(bill){
     console.log('Cart has the current bill, and it can use it as needed to generate markup!', bill);
-    var content = '';
+    let content = '';
 
     for(let i = 0; i < bill.length; i++){
         content += `
-        <main>
-<div class="shopping-cart">
-              <div class="shopping-cart-title">
-              Shopping Cart
-              </div>
+
     <div class="card" id="prod-${i + 1}">
         <div class="prod">
             <div id = 'delete-button'>
@@ -38,15 +34,23 @@ function itemAdder(bill){
             </div>
           </div>
         </div>
-      </div>
-    <div class="checkout-button">
-    <a href="./Checkout"> Proceed to Checkout </a>
-    </div>
-    </main>
-      `;
+      </div>`;
     }
 
     return content;
 }
 
-export default (state) => itemAdder(state.bill);
+export default (state) => `
+ <main>
+<div class="shopping-cart">
+              <div class="shopping-cart-title">
+              Shopping Cart
+              </div>
+
+${itemAdder(state.bill)}
+
+<div class="checkout-button">
+    <a href="./Checkout"> Proceed to Checkout </a>
+    </div>
+    </main>
+      `;
